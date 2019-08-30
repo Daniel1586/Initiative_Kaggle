4b1cc8150d978e3964a6ea6e620823b6cfde9425
Daniel1586 <33886608+Daniel1586@users.noreply.github.com>
2019-08-30 22:25:29 +0800

    update feature_fraud_detection.py

--- IEEE-CIS_Fraud_Detection/feature_fraud_detection.py
+++ IEEE-CIS_Fraud_Detection/feature_fraud_detection.py
@@ -199,7 +199,7 @@ def csv2csv(datain_dir, dataou_dir):
     res = pd.read_csv(dataou_dir + "\\infer.csv", header=None, names=["isFraud"])
     tests = idx.merge(res, how="left", left_index=True, right_index=True)
     tests_txt = tests.drop("ProductCD", axis=1)
-    tests_txt.to_csv(dataou_dir + "082902.csv", index=False, header=True)
+    tests_txt.to_csv(dataou_dir + "083001.csv", index=False, header=True)
 
 
 class CategoryDictGenerator:
